07de02d9929cada5c683c2e783471055094c9fc8
dohoeok <dohoseok@gmail.com>
2022-06-30 13:41:20 +0900

    Update data download script

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,5 @@
 wget -O dataset/download.zip https://kr.object.ncloudstorage.com/aidata-sample/20-02/2020-02-067.%EB%93%9C%EB%A1%A0%EB%86%8D%EA%B2%BD%EC%9E%91%EC%A7%80_sample.zip
 unzip dataset/download.zip -d dataset/
-mv dataset/[라벨]label dataset/label
-mv dataset/[원천]data dataset/image
+mv dataset/*label dataset/label
+mv dataset/*data dataset/image
 python build_np.py
